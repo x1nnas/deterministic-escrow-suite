3f0a00ef801fdaeefdd03fdfd7c23e09314478aa
Jose Ferreira <jmiguelf2000@gmail.com>
2026-03-25 14:57:23 +0000

    Completed Escrow Factory and test initialization

---
+++ test/EscrowFactory.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("EscrowFactory", function () {
+  it("should predict the correct address", async function () {
+    
+  });
+});
